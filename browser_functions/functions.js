b5164c957bb8aa91e80fd0af83719d2acf35a53b
ryanlondon <ryel590@gmail.com>
2018-01-24 17:30:48 -0800

    added browser color generator

---
+++ browser_functions/functions.js
@@ -0,0 +1,9 @@
+const browserGenerateRandomColor = () => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  return { red, green, blue };
+};
+
+export default browserGenerateRandomColor;
