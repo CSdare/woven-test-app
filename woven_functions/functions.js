db9b3279d89eb37b6e302c2828fc988c575b0394
ryanlondon <ryel590@gmail.com>
2018-01-24 17:31:21 -0800

    removed color generator

--- woven_functions/functions.js
+++ woven_functions/functions.js
@@ -8,12 +8,4 @@ const nthFib = (num) => {
   return nthFib(num - 1) + nthFib(num - 2);
 }
 
-const generateRandomColor = () => {
-  const red = Math.floor(Math.random() * 256);
-  const green = Math.floor(Math.random() * 256);
-  const blue = Math.floor(Math.random() * 256);
-
-  return { red, green, blue };
-};
-
-module.exports = { addTen, nthFib, generateRandomColor };
+module.exports = { addTen, nthFib };
