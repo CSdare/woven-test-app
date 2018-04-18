3546805d6811dfb1af4099527b5fcb971313a306
Erik Larsen <erik.w.larsen@gmail.com>
2018-04-17 22:49:59 -0400

    (update) remove commented out line

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const woven = require('woven-js'); // need to add npm module
-// const functions = require('./woven_functions/functions');
 
 app.use(bodyParser.json());
 // how can we incorporate bodyParser into our woven.optimize middleware?
